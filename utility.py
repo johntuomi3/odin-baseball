24a73a082bc83ff8a2487ae30bdc038d7d64ef91
John Tuomi III <jtuomi2@gmail.com>
2015-01-17 12:10:21 -0600

    Added OptionParser, Return Table as DataFrame
    Added functionality for initialization and returning a table as a Pandas
    DataFrame.

--- utility.py
+++ utility.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import csv
+from optparse import OptionParser
+from datetime import datetime
 import psycopg2
 import pandas as pd
 import numpy as np
@@ -9,12 +11,33 @@ from sqlalchemy import create_engine
 engine = create_engine("postgresql://postgres:Portage2@localhost/mlb")
 
 def main():
-    initialize()
+    opts, args = parseOptions()
+    print(opts, args)
+    if opts.initialize == True:
+        initialize()
+    elif opts.opt_table != False:
+        getSQLTable(opts.opt_table)
+
+
+def parseOptions():
+    parser = OptionParser()
+    parser.add_option("-i", "--initialize", help="Initialize and Load Baseball Data", action="store_true",
+                      dest="initialize", default=False)
+    parser.add_option("-t", "--table", help="Return a table from the database as a pandas DataFrame", 
+                      action="store", dest="opt_table", default=False)
+    (opts, args) = parser.parse_args()
+    return opts, args
 
 
 def initialize():
+    start_time = datetime.now()
     csv_file_list = getCSVFiles()
     parseCSV(csv_file_list)
+    end_time = datetime.now()
+    initialization_time = (end_time - start_time).strftime("%H:%M:%S")
+    print("Start Time: %s" % start_time)
+    print("End Time: %s" % end_time)
+    print("Elapsed Time: %s" % initialization_time)
 
 
 def getCSVFiles():
@@ -32,10 +55,16 @@ def parseCSV(csv_file_list):
     for csv_file_path in csv_file_list:
         table_name = str(csv_file_path.split("\\")[1]).replace('.csv', '')
         csv_file = pd.DataFrame.from_csv(csv_file_path)
-        print(csv_file)
-        print(table_name)
+        print("Importing CSV to table: ",table_name)
         csv_file.to_sql(table_name, engine,'postgresql',if_exists='replace')
         
+
+def getSQLTable(table):
+    df_table = pd.read_sql_table(table,engine)
+    print(df_table)
+    return df_table
+
+    
 
         
         
